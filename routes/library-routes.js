3189a52cab188c1f3d80d8a7b1c87bc32feac6ae
nimson07 <nimdiej055@gmail.com>
2025-03-12 14:06:07 +0000

    upload

--- routes/library-routes.js
+++ routes/library-routes.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { deleteBook, getBookById, getBooks, postBook, updateBook } from "../controllers/library-controllers.js";
+import { remoteUpload } from "../middlewares/upload.js";
 
 const bookRouter = Router();
 
-bookRouter.post('/book', postBook);
+bookRouter.post('/book',remoteUpload.single('image'), postBook);
 
 bookRouter.get('/books', getBooks);
 
 bookRouter.get('/book/:id', getBookById);
 
-bookRouter.patch('/book/:id', updateBook);
+bookRouter.patch('/book/:id', remoteUpload.single('image'), updateBook);
 
 bookRouter.delete('/book/:id', deleteBook);
 
